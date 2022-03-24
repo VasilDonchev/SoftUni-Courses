bd1a31cc90f15dbf8f74a1d3a0dfecf9023db613
Vasil Donchev <vasilkd@gmail.com>
2022-03-24 18:43:54 +0200

    update exercise 'GladiatorInventory'

--- JS Fundamentals/9. Arrays Advanced/Exercises/09.GladiatorInventory.js
+++ JS Fundamentals/9. Arrays Advanced/Exercises/09.GladiatorInventory.js
@@ -1,9 +1,45 @@
-function gladiatorInvertory(arr){
+function gladiatorInvertory(arr) {
+  let inventory = arr.shift().split(" ");
 
+  for (let el of arr) {
+    el.split(" ");
+    [command, equipment] = el.split(" ");
+    if (command == "Buy") {
+      if (!inventory.includes(equipment)) {
+        inventory.push(equipment);
+      }
+    } else if (command == "Trash") {
+      if (inventory.includes(equipment)) {
+        inventory.splice(inventory.indexOf(equipment), 1);
+      }
+    } else if (command == "Repair") {
+      if (inventory.includes(equipment)) {
+        inventory.splice(inventory.indexOf(equipment), 1);
+        inventory.push(equipment);
+      }
+    } else if (command == "Upgrade") {
+      [equipment, upgrade] = equipment.split("-");
+      if (inventory.includes(equipment)) {
+        let newWeapon = `${equipment}:${upgrade}`;
+        let index = inventory.indexOf(equipment);
+        inventory.splice(index + 1, 0, newWeapon);
+      }
+    }
+  }
+  console.log(inventory.join(" "));
 }
-gladiatorInvertory(['SWORD Shield Spear',
-'Buy Bag',
-'Trash Shield',
-'Repair Spear',
-'Upgrade SWORD-Steel']
-);
+
+gladiatorInvertory([
+  "SWORD Shield Spear",
+  "Buy Bag",
+  "Bay Bag",
+  "Trash Shield",
+  "Repair Spear",
+  "Upgrade SWORD-Steel",
+]);
+gladiatorInvertory([
+  "SWORD Shield Spear",
+  "Trash Bow",
+  "Repair Shield",
+  "Upgrade Helmet-V",
+]);
