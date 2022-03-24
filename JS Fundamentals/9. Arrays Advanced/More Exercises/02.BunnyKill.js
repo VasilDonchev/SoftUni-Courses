2871648fffd8d40ab094bb4e3223a0f9c7e271d5
Vasil Donchev <vasilkd@gmail.com>
2022-03-24 23:28:27 +0200

    solved exercises

--- JS Fundamentals/9. Arrays Advanced/More Exercises/02.BunnyKill.js
+++ JS Fundamentals/9. Arrays Advanced/More Exercises/02.BunnyKill.js
@@ -2,10 +2,29 @@ function bunnyKill(arr) {
   let bombs = arr.pop().split(" ");
   console.log(bombs);
   for (let index = 0; index < arr.length; index++) {
-    let el = arr.shift().split(" ");
+    let el = arr.shift().split(" ").map(Number);
     arr.push(el);
   }
-  
+  for (let el of bombs) {
+    [x, y] = el.split(',').map(Number);
+    let bomb = arr[x][y];
+    if (bomb > 0 && (x != 0 && y != 0)) {
+      arr[x-1][y-1] -= bomb;
+      arr[x][y-1] -= bomb;
+      arr[x+1][y-1] -= bomb;
+      arr[x-1][y] -= bomb;
+      arr[x][y] -= bomb;
+      arr[x+1][y] -= bomb;
+      arr[x-1][y+1] -= bomb;
+      arr[x][y+1] -= bomb;
+      arr[x+1][y+1] -= bomb;
+
+    } else if (bomb > 0 && (x == 0 && y > 0 ) {
+      
+    }{
+      
+    }
+  }
   console.log(arr);
 }
 bunnyKill([
