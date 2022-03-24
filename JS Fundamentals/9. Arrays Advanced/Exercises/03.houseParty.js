fe61217951d5156eea84215319cce69601fc7131
Vasil Donchev <vasilkd@gmail.com>
2022-03-24 22:13:44 +0200

    added new exercise

--- JS Fundamentals/9. Arrays Advanced/Exercises/03.houseParty.js
+++ JS Fundamentals/9. Arrays Advanced/Exercises/03.houseParty.js
@@ -1,4 +1,33 @@
-function houseParty(params) {
-    
+function houseParty(arr) {
+  let index = 0;
+  let guestComming = [];
+  for (let el of arr) {
+    let tokens = el.split(' ');
+    let name = tokens[0];
+    if (guestComming.includes(name) && tokens[2] == "going!") {
+      console.log(`${name} is already in the list!`);
+    } else if (guestComming.includes(name) && tokens[2] == "not") {
+      index = guestComming.indexOf(name);
+      guestComming.splice(index, 1);
+    } else if (!guestComming.includes(name) && tokens[2] == "going!") {
+      guestComming.push(name);
+    } else if (!guestComming.includes(name) && tokens[2] == "not") {
+      console.log(`${name} is not in the list!`);
+    }
+  }
+  for (let iterator of guestComming) {
+    console.log(iterator);
+  }
 }
-houseParty();
+// houseParty([
+//   "Allie is going!",
+//   "George is going!",
+//   "John is not going!",
+//   "George is not going!",
+// ]);
+houseParty(['Tom is going!',
+'Annie is going!',
+'Tom is going!',
+'Garry is going!',
+'Jerry is going!']
+);
