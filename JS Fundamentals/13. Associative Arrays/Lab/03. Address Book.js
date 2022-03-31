f2e1700820a2c2036acd8b14e9d56b61e1aa3cec
Vasil Donchev <vasilkd@gmail.com>
2022-03-31 10:20:09 +0300

    update exercises

--- JS Fundamentals/13. Associative Arrays/Lab/03. Address Book.js
+++ JS Fundamentals/13. Associative Arrays/Lab/03. Address Book.js
@@ -1,8 +1,31 @@
-function addressBook() {
-
+function addressBook(input) {
+    let addressbook = {};
+    for (let line of input) {
+      let [name, address] = line.split(':');
+      addressbook[name] = address;
+    }
+    let sortedArrey = Object.entries(addressbook);
+    sortedArrey.sort((a,b) => {
+        let keyA = a[0]
+        let keyB = b[0]
+        return keyA.localeCompare(keyB);
+    })
+    for (let sortedEntry of sortedArrey) {
+      console.log(`${sortedEntry[0]} -> ${sortedEntry[1]}`);
+    }
 }
 addressBook(['Tim:Doe Crossing',
 'Bill:Nelson Place',
 'Peter:Carlyle Ave',
-'Bill:Ornery Rd']
-);
+'Bill:Ornery Rd']);
+addressBook(['Bob:Huxley Rd',
+'John:Milwaukee Crossing',
+'Peter:Fordem Ave',
+'Bob:Redwing Ave',
+'George:Mesta Crossing',
+'Ted:Gateway Way',
+'Bill:Gateway Way',
+'John:Grover Rd',
+'Peter:Huxley Rd',
+'Jeff:Gateway Way',
+'Jeff:Huxley Rd']);
--- JS Fundamentals/13. Associative Arrays/Lab/04. Storage.js
+++ JS Fundamentals/13. Associative Arrays/Lab/04. Storage.js
@@ -1,4 +1,18 @@
-function storage() {
-
+function storage(arr) {
+  let stor = new Map();
+  for (let line of arr) {
+    line = line.split(" ");
+    let name = line[0];
+    let quantity = Number(line[1]);
+    if (stor.has(name)) {
+      let newQantity = stor.get(name) + quantity;
+      stor.set(name, newQantity);
+    } else {
+      stor.set(name, quantity);
+    }
+  }
+  for (let storEntry of stor) {
+    console.log(`${storEntry[0]} -> ${storEntry[1]}`);
+  }
 }
-storage
+storage(["tomatoes 10", "coffee 5", "olives 100", "coffee 40"]);
